c60d50cc4f36d0f6bf57fed5bef7549e133242d7
Taylor McGregor <mcgregor.taylor44@gmail.com>
2023-09-05 13:48:14 -0500

    made a class and properties

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -1,10 +1,13 @@
-﻿namespace Classes
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Classes
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var car1 = new Car("Lamborghini", "Urus", 2023);
+            Console.WriteLine($"The car I want is a {car1.Year} {car1.Make} {car1.Model}.");
         }
     }
 }
